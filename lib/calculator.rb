3e28dbc474899d1434d54fcda6cd9f3008c25e6a
alex549us3 <alex549us3@gmail.com>
2017-03-21 19:44:28 -0600

    initialize working and add working

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,18 @@
 class Calculator
+  attr_reader :total
+
+  def initialize
+    @total = 0
+  end
+
+  def total
+    @total
+  end
+
+  def add(a, b)
+    @total = a + b
+  end
+
+  
 
 end
--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -5,8 +5,21 @@ require './lib/calculator.rb'
 
 class CalculatorTest < Minitest::Test
 
+  def test_does_class_initialize
+    calc = Calculator.new
+    assert_instance_of Calculator, calc
+  end
 
-
-
+  def test_does_calc_init_with_total_zero
+    calc = Calculator.new
+    assert_equal 0, calc.total
+  end
+  
+  def test_can_add
+    calc = Calculator.new
+    calc.add(1, 1)
+    assert_equal calc.total, calc.add(1, 1)
+  end
+  
 
 end
